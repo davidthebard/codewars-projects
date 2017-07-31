c6747a060d4e33042d75264d1a6831a3a435b9e9
davidthebard <30606799+davidthebard@users.noreply.github.com>
2017-07-31 16:28:15 -0500

    Create Solution.java
    Kata can be found here: http://www.codewars.com/kata/street-fighter-2-character-selection/java

---
+++ Solution.java
@@ -0,0 +1,41 @@
+public class Solution{
+  public static String[] streetFighterSelection(String[][] fighters, int[] position, String[] moves){
+    String[] fighterList = new String[moves.length];
+    int currentX = 0;
+    int currentY = 0;
+    for(int i = 0; i < moves.length; i++) {
+      if (moves[i] == "up") {
+        if(currentY - 1 >= 0) {
+          currentY--;
+          fighterList[i] = fighters[currentY][(currentX)];
+        } else { 
+          fighterList[i] = fighters[currentY][(currentX)];
+        }
+      } else if(moves[i] == "down") {
+          if(currentY + 1 < 2) {
+            currentY++;
+            fighterList[i] = fighters[currentY][(currentX)];
+          } else {
+            fighterList[i] = fighters[currentY][currentX];
+          }
+      } else if(moves[i] == "left") {
+          if (currentX > 0) {
+            currentX--;
+            fighterList[i] = fighters[currentY][(currentX)]; 
+          } else {
+            currentX = 5;
+            fighterList[i] = fighters[currentY][currentX];
+          }
+      } else if(moves[i] == "right") {
+          if (currentX < 5) {
+            currentX++;
+            fighterList[i] = fighters[(currentY)][(currentX)];
+          } else {
+            currentX = 0;
+            fighterList[i] = fighters[currentY][currentX];
+          }
+      }
+    }
+    return fighterList;
+  }
+}
